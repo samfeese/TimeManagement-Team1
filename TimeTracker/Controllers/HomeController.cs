c988bbed40f8df94fdd211385b9811ee0631f458
LUke <porterl2268@student.faytechcc.edu>
2019-09-24 12:09:40 -0400

    got stopwatch working, parsing issue for seconds

--- TimeTracker/Controllers/HomeController.cs
+++ TimeTracker/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -12,6 +13,20 @@ namespace TimeTracker.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+
+        bool started = false;
+
+        bool ended = false;
+
+        static Stopwatch stopwatch = new Stopwatch();
+
+        static string stopwatchString;
+
+        static string[] stopwatchList;
+
+        double currentTotal;
+
+        double total;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -25,15 +40,44 @@ namespace TimeTracker.Controllers
 
         public IActionResult Start()
         {
+            stopwatch.Start();
             ViewBag.Start = true;
+            started = true;
             ViewBag.End = false;
+            ended = false;
             return View("Index");
         }
 
         public IActionResult End()
         {
+            if (started || ended == false && started == false)
+            {
+                stopwatch.Stop();
+                stopwatchString = stopwatch.Elapsed.ToString();
+                stopwatchList = stopwatchString.Split(":");
+                var trimIndex = stopwatchList[2].IndexOf(".");
+                var hours = Convert.ToInt32(stopwatchList[1]);
+                var minutes = Convert.ToInt32(stopwatchList[2].Remove(trimIndex - 1, stopwatchList[2].Length));
+                if(Convert.ToInt32(stopwatchList[1]) > 0)
+                {
+                    currentTotal = Convert.ToInt32(stopwatchList[1])* 60 + Convert.ToInt32(stopwatchList[2]);
+                }
+                else if (Convert.ToInt32(stopwatchList[2].Remove(trimIndex, stopwatchList.Length)) < 5)
+                {
+                    currentTotal = 5;
+                }
+                else
+                {
+                    currentTotal = Convert.ToInt32(stopwatchList[2]);
+                }
+                ViewBag.Current = currentTotal;
+                this.total += currentTotal;
+                ViewBag.Total = total;
+            }
             ViewBag.Start = false;
+            started = false;
             ViewBag.End = true;
+            ended = true;
             return View("Index");
         }
 
