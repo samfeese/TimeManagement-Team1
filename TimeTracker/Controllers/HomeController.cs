cc16427d71b0fe051e004df67f1992061d1e7841
LUke <porterl2268@student.faytechcc.edu>
2019-10-04 08:20:48 -0400

    fixed time logic

--- TimeTracker/Controllers/HomeController.cs
+++ TimeTracker/Controllers/HomeController.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TimeTracker.Models;
@@ -14,23 +10,8 @@ namespace TimeTracker.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        bool started = false;
+        TimeTracking timeTracker = new TimeTracking();
 
-        bool ended = false;
-
-        public Stopwatch stopwatch;
-
-        static DateTime startTime;
-
-        static DateTime endTime;
-
-        static string stopwatchString;
-
-        static string[] stopwatchList;
-
-        double currentTotal;
-
-        double total;
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -45,57 +26,37 @@ namespace TimeTracker.Controllers
 
         public IActionResult Start()
         {
-            stopwatch = new Stopwatch();
-            stopwatch.Start();
-            startTime = DateTime.Now;
-            ViewBag.Start = true;
-            started = true;
-            ViewBag.End = false;
-            ended = false;
+            timeTracker.Start();
+            var (startTime, isStart, isEnd) = timeTracker.Start();
+            ViewBag.Start = isStart;
+            ViewBag.End = isEnd;
+            ViewBag.StartTime = startTime;
             return View("Index");
         }
 
         public IActionResult End()
         {
-            var user = User.Identity.Name;
-            var permission = User.IsInRole("admin");
-            
-            if (started || ended == false && started == false)
-            {
-                stopwatch.Stop();
 
-                endTime = DateTime.Now;
+            //var user = User.Identity.Name;
+            //var permission = User.IsInRole("admin");
+
+            try
+            {
+                var (startTime, endTime, started, ended,
+                currentTotal, total) = timeTracker.End();
                 ViewBag.StartTime = startTime;
                 ViewBag.EndTime = endTime;
-                var totalTime = stopwatch.Elapsed;
-                stopwatchString = stopwatch.Elapsed.ToString();
-                stopwatchList = stopwatchString.Split(":");
-                var trimIndex = stopwatchList[2].IndexOf(".");
-                var hours = Convert.ToInt32(stopwatchList[1]);
-                var minutes = Convert.ToInt32(stopwatchList[2].Remove(trimIndex - 1, stopwatchList[2].Length - 1));
-
-
-                if (hours > 0)
-                {
-                    currentTotal = Convert.ToInt32(hours) * 60 + Convert.ToInt32(minutes);
-                }
-                else if (minutes < 5)
-                {
-                    currentTotal = 5;
-                }
-                else
-                {
-                    currentTotal = Convert.ToInt32(stopwatchList[2]);
-                }
                 ViewBag.Current = currentTotal;
-                this.total += currentTotal;
                 ViewBag.Total = total;
+                ViewBag.Start = started;
+                ViewBag.End = ended;
             }
-            ViewBag.Start = false;
-            started = false;
-            ViewBag.End = true;
-            ended = true;
-            return View();
+            catch (Exception)
+            {
+            }
+
+            return View("Index");
+
         }
         public IActionResult Privacy()
         {
---
+++ TimeTracker/Models/TimeTracking.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeTracker.Models
+{
+    public class TimeTracking
+    {
+        bool started = false;
+
+        bool ended = false;
+
+        static Stopwatch stopwatch = new Stopwatch();
+
+        static DateTime startTime;
+
+        static DateTime endTime;
+
+        static string stopwatchString;
+
+        static string[] stopwatchList;
+
+        double currentTotal;
+
+        double total;
+
+        public (DateTime, bool, bool) Start()
+        {
+            stopwatch.Start();
+            startTime = DateTime.Now;
+            started = true;
+            ended = false;
+            return (startTime, started, ended);
+        }
+
+        public (DateTime, DateTime, bool, bool, double, double) End()
+        {
+
+            if (started || ended == false && started == false)
+            {
+                stopwatch.Stop();
+
+                endTime = DateTime.Now;
+                var totalTime = stopwatch.Elapsed;
+                stopwatchString = stopwatch.Elapsed.ToString();
+                stopwatchList = stopwatchString.Split(":");
+                var trimIndex = stopwatchList[2].IndexOf(".");
+                var hours = Convert.ToInt32(stopwatchList[1]);
+                var minutes = Convert.ToInt32(stopwatchList[2].Remove(trimIndex - 1, stopwatchList[2].Length - 1));
+
+
+                if (hours > 0)
+                {
+                    currentTotal = Convert.ToInt32(hours) * 60 + Convert.ToInt32(minutes);
+                }
+                else if (minutes < 5)
+                {
+                    currentTotal = 5;
+                }
+                else
+                {
+                    currentTotal = Convert.ToInt32(stopwatchList[2]);
+                }
+                this.total += currentTotal;
+                started = false;
+                ended = false;
+                stopwatch.Reset();
+                return (startTime, endTime, started, ended, currentTotal, total);
+            }
+            else
+            {
+                throw new Exception("Invalid time function");
+            }
+        }
+    }
+}
